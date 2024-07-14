ca8786c692714a8a22b5cb2477a29847b768739a
Bilius10 <jvdrdo@gmail.com>
2024-07-14 14:47:19 -0300

    first commit

---
+++ funcoes.py
@@ -0,0 +1,230 @@
+import pandas as pd
+import flet as ft
+
+#funcao que lista a tabela armazem
+def listar_armazem(page, voltar_func):
+    # Carrega a tabela do arquivo Excel
+    tabela_armazem = pd.read_excel("armazem.xlsx")
+    # Limpa a página
+    page.clean()
+    # Cria as colunas da tabela
+    columns = [ft.DataColumn(ft.Text(col)) for col in tabela_armazem.columns]
+    # Cria as linhas da tabela
+    rows = [ft.DataRow(cells=[ft.DataCell(ft.Text(str(tabela_armazem[col].iloc[i]))) for col in tabela_armazem.columns])
+            for i in range(len(tabela_armazem))]
+    # Cria a tabela de dados
+    data_table = ft.DataTable(columns=columns, rows=rows, border=ft.border.all(1))
+    # Adiciona rolagem à tabela usando um contêiner Column
+    scrollable_table = ft.Column([data_table], scroll=ft.ScrollMode.ALWAYS, height=300)
+    # Cria o botão "Voltar"
+    botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar_func), alignment=ft.alignment.bottom_right, margin=10)
+    # Adiciona a tabela rolável e o botão "Voltar" na página
+    page.add(scrollable_table, botao_voltar)
+    
+#funcao que lista a tabela kit
+def listar_kits(page, voltar_func):
+    # Carrega a tabela do arquivo Excel
+    tabela_kit = pd.read_excel("kit.xlsx")
+    # Limpa a página
+    page.clean()
+    # Cria as colunas da tabela
+    columns = [ft.DataColumn(ft.Text(col)) for col in tabela_kit.columns]
+    # Cria as linhas da tabela
+    rows = [ft.DataRow(cells=[ft.DataCell(ft.Text(str(tabela_kit[col].iloc[i]))) for col in tabela_kit.columns]
+                       ) for i in range(len(tabela_kit))]
+     # Cria a tabela de dados
+    data_table = ft.DataTable(columns=columns,rows=rows,border=ft.border.all(1),)
+    # Adiciona rolagem à tabela usando um contêiner Column
+    scrollable_table = ft.Column([data_table], scroll=ft.ScrollMode.ALWAYS, height=300)
+    # Cria o botão "Voltar"
+    botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar_func), alignment=ft.alignment.bottom_right, margin=10)
+    # Adiciona a tabela rolável e o botão "Voltar" na página
+    page.add(scrollable_table, botao_voltar)
+    
+#funcao que lista a tabela vendas
+def listar_vendas(page, voltar_func):
+    # Carrega a tabela do arquivo Excel
+    tabela_vendas = pd.read_excel("Saida dos produtos.xlsx")
+
+    # Limpa a página
+    page.clean()
+
+    # Cria as colunas da tabela
+    columns = [ft.DataColumn(ft.Text(col)) for col in tabela_vendas.columns]
+
+     # Cria as linhas da tabela
+    rows = [ft.DataRow(cells=[ft.DataCell(ft.Text(str(tabela_vendas[col].iloc[i]))) for col in tabela_vendas.columns]
+                       ) for i in range(len(tabela_vendas))]
+    
+    # Cria a tabela de dados
+    data_table = ft.DataTable(columns=columns,rows=rows,border=ft.border.all(1),)
+
+    # Adiciona rolagem à tabela usando um contêiner Column
+    scrollable_table = ft.Column([data_table], scroll=ft.ScrollMode.ALWAYS, height=300)
+
+    # Cria o botão "Voltar"
+    botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar_func), alignment=ft.alignment.bottom_right, margin=10)
+
+    # Adiciona a tabela rolável e o botão "Voltar" na página
+    page.add(scrollable_table, botao_voltar)
+    
+#funcao que cadastra o produto
+def cadastro_produto(codigo, nome, quantidade, valor):
+    # Carrega a tabela do arquivo Excel
+    tabela_armazem = pd.read_excel("armazem.xlsx")
+
+    #cria um df inserir e insere as informações recebidas
+    inserir = pd.DataFrame({
+        "codigo": [codigo],
+        "nome": [nome],
+        "quantidade": [quantidade],
+        "valor": [valor * quantidade]
+    })
+
+    #faz uma verificação se ja existe esse item na tebal, caso tenha ele salva em cima desse item 
+    #verifica se o codigo recebido ja tem na tabel
+    if inserir["codigo"].values[0] in tabela_armazem['codigo'].values:
+
+        #verifica a linha que se localiza esse item
+        indice = tabela_armazem[tabela_armazem['codigo'] == inserir["codigo"].values[0]].index[0]
+        #soma com a quantidade e o valor atual
+        tabela_armazem.loc[indice, 'quantidade'] += inserir["quantidade"].values[0]
+        tabela_armazem.loc[indice, 'valor'] += inserir.loc[0, 'valor']
+    else:
+        #caso não tenha ele apenas concatena com as ultimas informações
+        tabela_armazem = pd.concat([tabela_armazem, inserir], ignore_index=True)
+
+    try:
+        #tenta salvar, caso ele sava retorna uma mensagem
+        tabela_armazem.to_excel('armazem.xlsx', index=False)
+        return "Cadastro realizado com sucesso!"
+    except Exception as e:
+        #caso não tenha, ele retorna uma mensagem com o erro
+        return f"Erro ao salvar: {e}"
+
+#funcao que cadastra o kit
+def cadastro_kit(codigo, nome, produto, valor):
+    # Carrega a tabela do arquivo Excel
+    tabela_kit = pd.read_excel("kit.xlsx")
+
+    # Criação do DataFrame com os dados do novo kit
+    inserir = pd.DataFrame({
+        "codigo": [codigo],
+        "kit": [nome],
+        "produto": [produto],
+        "valor": [valor]
+    })
+
+    # Concatena com a tabela existente
+    tabela_kit = pd.concat([tabela_kit, inserir], ignore_index=True)
+
+    try:
+        # Salvar a tabela atualizada em um arquivo Excel, e retorna uma mensagem
+        tabela_kit.to_excel('kit.xlsx', index=False)
+        return "Cadastro realizado com sucesso!"
+    except Exception as e:
+        #caso de erro, retorna o erro
+        return f"Erro ao salvar: {e}"
+
+#Funcao que cadastra as vendas
+def cadastro_vendas(nome, quantidade, valor, nome_cliente):
+    #carrega as tabelas do excel
+    tabela_vendas = pd.read_excel("Saida dos produtos.xlsx")
+    tabela_armazem = pd.read_excel("armazem.xlsx")
+
+    from datetime import datetime
+    #inserção das informaões dentro do df
+    inserir = pd.DataFrame({"nome": [nome],
+                            "quantidade": [quantidade],
+                            "valor": [valor],
+                            "nome_cliente": [nome_cliente],
+                            "data": [datetime.now().strftime('%d/%m/%Y')]})   
+    
+    #concatenar com as ultimas informações
+    tabela_vendas = pd.concat([tabela_vendas, inserir], ignore_index=True) 
+
+    #procura a a linha que se localiza o produto na tebela armazem
+    indice = tabela_armazem[tabela_armazem['nome'] == inserir.loc[0,'nome']].index[0]
+
+    #Atualize a quantidade e o valor no DataFrame tabelaArmazem
+    retirar = tabela_armazem.loc[indice, 'valor'].astype(float)
+    retirar -= inserir.loc[0, 'valor'].astype(float)
+    tabela_armazem.loc[indice, 'valor'] = retirar.astype(float)
+    tabela_armazem.loc[indice, 'quantidade'] -= inserir.loc[0, 'quantidade'].astype(float)
+
+    #verificação de salvamento
+    try:
+        # Salvar a tabela atualizada em um arquivo Excel, e retorna uma mensagem
+        tabela_vendas.to_excel('Saida dos produtos.xlsx', index=False)
+        tabela_armazem.to_excel('armazem.xlsx', index=False)
+        return "Cadastro realizado com sucesso!"
+    except Exception as e:
+        #retorna uma mensagem de erro, que retorna o erro
+        return f"Erro ao salvar: {e}"
+
+#funcao que retira itens da tabela armazen
+def eliminar(codigo):
+    #carrega as tabelas do excel
+    tabela_armazem = pd.read_excel("armazem.xlsx")
+
+    #verifica se o codigo esta no df
+    if codigo in tabela_armazem['codigo'].values:
+        #procura a posição desse valor dentro
+        indice = tabela_armazem[tabela_armazem['codigo'] == codigo].index[0]
+        #retira o item do dataframe
+        tabela_armazem = tabela_armazem.drop(indice)
+
+        #verificação de salvamento no excel
+        try:
+            #salva e retorna uma mensagem
+            tabela_armazem.to_excel('armazem.xlsx', index=False)
+            return "Produto retirado"
+        except Exception as e:
+            #retorna uma mensagem de erro, que retorna o erro
+            return f"Erro ao salvar: {e}"
+
+    #caso não exista, retorna essa mensagem         
+    else:
+        return "Produto não existente"
+    
+#funcao que procura itens dentro da tabela armazen
+def procurar(codigo, page, voltar_func):
+    #carrega a tabela do excel
+    tabela_armazem = pd.read_excel("armazem.xlsx")
+
+    # Busca o item dentro da coluna codigo do df
+    if codigo in tabela_armazem['codigo'].values:
+        # Localiza a posição do item
+        indice = tabela_armazem[tabela_armazem['codigo'] == codigo].index[0]
+        
+        # Obtém os dados do item encontrado
+        item = tabela_armazem.loc[indice]
+
+        # Cria a tabela para exibir o item encontrado
+        columns = [
+            ft.DataColumn(ft.Text('codigo')),
+            ft.DataColumn(ft.Text('nome')),
+            ft.DataColumn(ft.Text('quantidade')),
+            ft.DataColumn(ft.Text('valor'))
+        ]
+
+        rows = [
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(str(item['codigo']))),
+                    ft.DataCell(ft.Text(item['nome'])),
+                    ft.DataCell(ft.Text(str(item['quantidade']))),
+                    ft.DataCell(ft.Text(str(item['valor'])))
+                ]
+            )
+        ]
+
+        # Limpa a página e adiciona a nova tabela e botão de voltar
+        page.controls.clear()
+        botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar_func), alignment=ft.alignment.bottom_right, margin=10)
+        page.add(ft.DataTable(columns=columns, rows=rows), botao_voltar)
+
+    else:
+        return "Produto não existe"
+        
+        
---
+++ teste.py
@@ -0,0 +1,239 @@
+import flet as ft
+import funcoes
+import time
+
+# Armazenamento do histórico de navegação
+navigation_history = []
+
+#Funcao mãe, basicamente tudo que estiver dentro dela ira ser o aplicativo
+def main(page: ft.Page):
+    #configurações da tela de principal
+    page.title = "SISTEMA DE ARMAZENAMENTO"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.theme = ft.Theme(color_scheme_seed=ft.colors.GREY)
+    
+    #função salva o estado atual dos controles da página,
+    def salvar_estado_atual():
+        #salva em uma lista o que tem naquela pagina ou seja, o estado atual
+        estado_atual = {
+            'controls': list(page.controls)
+        }
+        #o que esta dentro da lista estado_atual, sera salvo na lista navigation_history
+        navigation_history.append(estado_atual)
+        #limpara o que estava na ultima pagina acessada, para não haver sobreposição do novo layout com o antigo 
+        page.clean()
+
+    #função que sera usada para voltar para a ultima pagina acessada
+    def voltar(evento):
+        if navigation_history:
+            #dara um pop"retirar" as ultimas informações salvas "sera a pagina que estaremos no exato momento"
+            last_state = navigation_history.pop()
+            #limpara a pagina
+            page.clean()
+            #voltara para a ultima pagina salva na lista
+            for control in last_state['controls']:
+                #adiciona os itens a pagina
+                page.add(control)
+            
+    #funcao que sera chama ao clicar o botao"Cadastrar Produto" localizado no menu
+    def cadastro_produto(evento):
+
+        #usa a funcao para salvar as configurações da pagina"spawpoint"
+        salvar_estado_atual()
+
+        #cria TextField"caixas de texto flutuantes", para escrevermos nelas 
+        codigo = ft.TextField(label="Código do Produto", width=200)
+        nome = ft.TextField(label="Nome do Produto", width=200)
+        quantidade = ft.TextField(label="Quantidade", width=200)
+        valor = ft.TextField(label="Valor Unitário", width=200)
+
+        #funcao aonde inviaremos as informações escritas a uma outra funcao
+        def enviar(evento):
+            #enviamos as informações recebidas para a funcao cadastro_produto
+            mensagem = funcoes.cadastro_produto(int(codigo.value),str(nome.value),int(quantidade.value),float(valor.value))
+            #adiciona a mensagem recebida na tela/e apos meio segundo retira ela
+            men_tela =ft.Text(mensagem)
+            page.add(men_tela)
+            time.sleep(0.5)
+            page.remove(men_tela)
+            
+
+        #parte responsavel por criar os itens que apareceram na tela
+        titulo = ft.Container(content=ft.Text("CADASTRO DE PRODUTOS", size=24, weight=ft.FontWeight.BOLD, color=ft.colors.GREY), alignment=ft.alignment.center, margin=10)
+        botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar), alignment=ft.alignment.bottom_right, margin=10)
+        botao_cadastro = ft.ElevatedButton(text="Cadastrar", on_click=enviar)
+        #form estara salvando os TextField criados um pouco acima
+        form = ft.Column([codigo, nome, quantidade, valor], alignment=ft.MainAxisAlignment.CENTER)
+        #aqui adicionamos os itens na tela 
+        page.add(titulo, form, botao_voltar, botao_cadastro)
+        
+    #funcao que sera chama ao clicar o botao"Cadastrar Kit" localizado no menu
+    def cadastro_kit(evento):
+
+        #usa a funcao para salvar as configurações da pagina"spawpoint"
+        salvar_estado_atual()
+
+        #cria TextField"caixas de texto flutuantes", para escrevermos nelas 
+        codigo = ft.TextField(label="Código do Kit", width=300)  
+        kit = ft.TextField(label="Nome do Kit", width=300)  
+        produto = ft.TextField(label="Nomes dos produtos que estarão no kit", width=300)  
+        valor = ft.TextField(label="Valor Unitário", width=300)  
+
+        #funcao aonde inviaremos as informações escritas a uma outra funcao
+        def enviar(evento):
+                #enviamos as informações recebidas para a funcao cadastro_produto
+                mensagem = funcoes.cadastro_kit(int(codigo.value), str(kit.value), str(produto.value), float(valor.value))
+                #adiciona a mensagem recebida na tela/e apos meio segundo retira ela
+                men_tela =ft.Text(mensagem)
+                page.add(men_tela)
+                time.sleep(0.5)
+                page.remove(men_tela)
+
+        #parte responsavel por criar os itens que apareceram na tela
+        botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar), alignment=ft.alignment.bottom_right, margin=10)
+        titulo = ft.Container(content=ft.Text("CADASTRO DE KITS", size=24, weight=ft.FontWeight.BOLD, color=ft.colors.GREY), alignment=ft.alignment.center, margin=10)
+        botao_cadastro = ft.ElevatedButton(text="Cadastrar", on_click=enviar)
+        #form estara salvando os TextField criados um pouco acima
+        form = ft.Column([codigo, kit, produto, valor], alignment=ft.MainAxisAlignment.CENTER)
+        #aqui adicionamos os itens na tela 
+        page.add(titulo, form, botao_voltar, botao_cadastro)
+
+    #funcao que sera chama ao clicar o botao"Cadastro Venda" localizado no menu
+    def cadastro_venda(evento):
+
+        #usa a funcao para salvar as configurações da pagina"spawpoint"
+        salvar_estado_atual()
+
+        #cria TextField"caixas de texto flutuantes", para escrevermos nelas 
+        nome = ft.TextField(label="Digite o nome do produto", width=300) 
+        quantidade = ft.TextField(label="Quantidade vendida", width=300)  
+        valor = ft.TextField(label="Digite o valor total da venda", width=300) 
+        nome_cliente = ft.TextField(label="Digite o nome do cliente", width=300)  
+
+        #funcao aonde inviaremos as informações escritas a uma outra funcao
+        def enviar(evento):
+                #enviamos as informações recebidas para a funcao cadastro_produto
+                mensagem = funcoes.cadastro_vendas(str(nome.value), int(quantidade.value), float(valor.value), str(nome_cliente.value))
+                #adiciona a mensagem recebida na tela/e apos meio segundo retira ela
+                men_tela =ft.Text(mensagem)
+                page.add(men_tela)
+                time.sleep(0.5)
+                page.remove(men_tela)
+                
+        #parte responsavel por criar os itens que apareceram na tela
+        botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar), alignment=ft.alignment.bottom_right, margin=10)
+        titulo = ft.Container(content=ft.Text("CADASTRO DE VENDAS", size=24, weight=ft.FontWeight.BOLD, color=ft.colors.GREY), alignment=ft.alignment.center, margin=10)
+        botao_cadastro = ft.ElevatedButton(text="Cadastrar", on_click=enviar)
+        #form estara salvando os TextField criados um pouco acima 
+        form = ft.Column([nome, quantidade, valor, nome_cliente], alignment=ft.MainAxisAlignment.CENTER)
+        #aqui adicionamos os itens na tela 
+        page.add(titulo, form, botao_voltar, botao_cadastro)
+
+    #funcao que sera chama ao clicar o botao"Retirar Produto" localizado no menu
+    def retirar(evento):
+
+        #usa a funcao para salvar as configurações da pagina"spawpoint"
+        salvar_estado_atual()
+
+        #cria TextField"caixas de texto flutuantes", para escrevermos nelas 
+        codigo = ft.TextField(label="Código do produto", width=300)
+
+        #funcao aonde inviaremos as informações escritas a uma outra funcao
+        def enviar(evento):
+                #enviamos as informações recebidas para a funcao cadastro_produto
+                mensagem = funcoes.eliminar(int(codigo.value))
+                #adiciona a mensagem recebida na tela/e apos meio segundo retira ela
+                men_tela =ft.Text(mensagem)
+                page.add(men_tela)
+                time.sleep(0.5)
+                page.remove(men_tela)
+
+            
+           
+        #parte responsavel por criar os itens que apareceram na tela
+        titulo = ft.Container(content=ft.Text("RETIRAR PRODUTO", size=24, weight=ft.FontWeight.BOLD, color=ft.colors.GREY), alignment=ft.alignment.center, margin=10)
+        botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar), alignment=ft.alignment.bottom_right, margin=10)
+        botao_retirar = ft.ElevatedButton(text="Retirar", on_click=enviar)
+        #aqui adicionamos os itens na tela 
+        page.add(titulo, botao_voltar, codigo, botao_retirar)
+
+    #funcao que sera chama ao clicar o botao "Pesquisar Produto" localizado no menu
+    def pesquisar(evento):
+
+        #usa a funcao para salvar as configurações da pagina"spawpoint"
+        salvar_estado_atual()
+
+        #cria TextField"caixas de texto flutuantes", para escrevermos nelas 
+        codigo = ft.TextField(label="Código do produto", width=300)
+
+        #funcao aonde inviaremos as informações escritas a uma outra funcao
+        def enviar(evento):
+                #enviamos as informações recebidas para a funcao cadastro_produto
+                mensagem = funcoes.procurar(int(codigo.value), page, voltar)
+                #adiciona a mensagem recebida na tela/e apos meio segundo retira ela
+                men_tela =ft.Text(mensagem)
+                page.add(men_tela)
+                time.sleep(0.5)
+                page.remove(men_tela)
+
+
+        #parte responsavel por criar os itens que apareceram na tela
+        titulo = ft.Container(content=ft.Text("LISTAR TABELAS", size=24, weight=ft.FontWeight.BOLD, color=ft.colors.GREY), alignment=ft.alignment.center, margin=10)
+        botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar), alignment=ft.alignment.bottom_right, margin=10)
+        botao_procurar = ft.ElevatedButton(text="Procurar", on_click=enviar)
+        #aqui adicionamos os itens na tela 
+        page.add(titulo, botao_voltar, codigo, botao_procurar)
+
+    #funcao que sera chama ao clicar o botao "Listar Tabelas" localizado no menu
+    def listar(evento):
+
+        #usa a funcao para salvar as configurações da pagina"spawpoint"
+        salvar_estado_atual()
+
+        #cria o titulo da pagina 
+        titulo = ft.Container(content=ft.Text("LISTAR TABELAS", size=24, weight=ft.FontWeight.BOLD, color=ft.colors.GREY), alignment=ft.alignment.center, margin=10)
+
+        #cria os botoes/ dentro de cada ao clicar eles sera puxado uma funcao
+        tabela_1 = ft.Container(content=ft.ElevatedButton(text="Listar Armazem",  on_click=lambda e: funcoes.listar_armazem(page, voltar)), margin=10, alignment=ft.alignment.center,)
+        tabela_2 = ft.Container(content=ft.ElevatedButton(text="Listar kits",  on_click=lambda e: funcoes.listar_kits(page, voltar)), margin=10, alignment=ft.alignment.center,)
+        tabela_3 = ft.Container(content=ft.ElevatedButton(text="Listar Vendas",  on_click=lambda e: funcoes.listar_vendas(page, voltar)), margin=10, alignment=ft.alignment.center,)
+        botao_voltar = ft.Container(content=ft.ElevatedButton(text="Voltar", on_click=voltar), alignment=ft.alignment.bottom_right, margin=10)
+
+        #adiciona os itens na tela
+        page.add(titulo, tabela_1, tabela_2, tabela_3, botao_voltar)
+    
+    #funcao que sera chama ao clicar o botao "Sair" localizado no menu
+    def sair(evento):
+        #fecha a pagina
+        page.window_close()
+
+    #criação do menu
+    page.add(
+        ft.Column(
+            [
+                #cricao do titulo
+                ft.Container(
+                    content=ft.Text("GERENCIADOR DE ARMAZENTO", size=32, weight=ft.FontWeight.BOLD, color=ft.colors.GREY),
+                    alignment=ft.alignment.center,
+                    margin=10
+                ),
+                #criação dos botoes, aonde cada um que voce clica sera direcionado ao uma funcao
+                ft.Container(content=ft.ElevatedButton(text="Cadastrar Produto", on_click=cadastro_produto), margin=10, alignment=ft.alignment.center,),
+                ft.Container(content=ft.ElevatedButton(text="Cadastrar Kit", on_click=cadastro_kit), margin=10, alignment=ft.alignment.center,),
+                ft.Container(content=ft.ElevatedButton(text="Cadastrar Venda", on_click=cadastro_venda), margin=10, alignment=ft.alignment.center,),
+                ft.Container(content=ft.ElevatedButton(text="Retirar Produto", on_click=retirar), margin=10, alignment=ft.alignment.center,),
+                ft.Container(content=ft.ElevatedButton(text="Pesquisar Produto", on_click=pesquisar), margin=10, alignment=ft.alignment.center,),
+                ft.Container(content=ft.ElevatedButton(text="Listar Tabelas", on_click=listar), margin=10, alignment=ft.alignment.center,),
+                ft.Container(content=ft.ElevatedButton(text="Sair", on_click=sair), margin=10, alignment=ft.alignment.center,)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+        )
+    )
+
+#essa parte inicia o app
+ft.app(target=main)
+
+
+
